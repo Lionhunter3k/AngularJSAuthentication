0bc7a56b5c0e7f9387ad55fecbf8b0c9de927584
Lionhunter3k <lionhunter@gmail.com>
2019-04-24 03:20:35 +0300

    other additions

---
+++ OAuthTutorial/Controllers/OAuthClientsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using nH.Identity.Core;
+using NHibernate;
+using OAuthTutorial.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NHibernate.Linq;
+using OAuthTutorial.Models.OAuthClientsViewModels;
+
+namespace OAuthTutorial.Controllers
+{
+    [Authorize]
+    public class OAuthClientsController : Controller
+    {
+        private readonly ISession _context;
+        private readonly UserManager<User> _userManager;
+
+        public OAuthClientsController(ISession context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: OAuthClients
+        public async Task<IActionResult> Index()
+        {
+            string uid = _userManager.GetUserId(this.User);
+            return View(await _context.Query<OAuthClient>().Where(x => x.Owner.Id == uid).Fetch(x => x.Owner).ToListAsync());
+        }
+
+        // POST: OAuthClients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClientName,ClientDescription")] CreateClientViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var owner = await _userManager.GetUserAsync(this.User);
+                var client = new OAuthClient()
+                {
+                    ClientDescription = vm.ClientDescription,
+                    ClientName = vm.ClientName,
+                    ClientId = Guid.NewGuid().ToString(),
+                    ClientSecret = Guid.NewGuid().ToString(),
+                    Owner = owner,
+                };
+
+                await _context.SaveAsync(client);
+                await _context.FlushAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vm);
+        }
+
+        // GET: OAuthClients/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            string uid = _userManager.GetUserId(this.User);
+            var oAuthClient = await _context.Query<OAuthClient>().Fetch(x => x.Owner).FetchMany(x => x.RedirectURIs)
+                .SingleOrDefaultAsync(m => m.ClientId == id && m.Owner.Id == uid);
+            if (oAuthClient == null)
+            {
+                return NotFound();
+            }
+
+            EditClientViewModel vm = new EditClientViewModel()
+            {
+                ClientName = oAuthClient.ClientName,
+                ClientDescription = oAuthClient.ClientDescription,
+                ClientId = oAuthClient.ClientId,
+                ClientSecret = oAuthClient.ClientSecret,
+                RedirectUris = oAuthClient.RedirectURIs.ToArray()
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ OAuthTutorial/Models/OAuthClientsViewModels/EditClientViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuthTutorial.Models.OAuthClientsViewModels
+{
+    public class EditClientViewModel
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(500)]
+        public string ClientDescription { get; set; }
+
+        public string ClientName { get; internal set; }
+
+        public string ClientId { get; internal set; }
+
+        public string ClientSecret { get; internal set; }
+
+        public string[] RedirectUris { get; set; } = new string[0];
+    }
+}
